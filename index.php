b7d93e474b372498a702b99372b80b6197153390
Lukas Köhler <lukas.koehler@ueberbit.de>
2023-11-07 10:24:35 +0100

    Working template

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
-# Include Autoloader
-include 'Autoloader.php';
-
+# Create and use Autoloader
+function autoload($class){$class = str_replace('\\', '/', $class);require_once './' . $class . '.php';}
+spl_autoload_register('autoload');
 
 # Specify JSON Header and encoding
 header('Content-Type: application/json; charset=utf-8');
 
-$err = new \Classes\handlers\ErrorHandler();
-$err->test();
+# Parse Path
+$path = explode("/", $_SERVER['REQUEST_URI']);
+array_shift($path);
+
+# Case switch determines Endpoint, and passes the path to the respective Controller
+switch ($path[0]) {
+    case 'users':
+        print("user endpoint");
+        break;
+    case 'documents':
+        print("document endpoint");
+        break;
+}
